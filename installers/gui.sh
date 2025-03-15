e85c7b6251d53d768cf01aa6ac4a26398be90b47
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-03-15 12:25:59 +0300

    исправил пакеты для gui

--- installers/gui.sh
+++ installers/gui.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-sudo yay -S \
+yay -S \
   catppuccin-cursors-mocha \
   catppuccin-gtk-theme-mauve \
+  dunst \
   feh \
-  fontawesome-fonts \
-  fontawesome-fonts-web \
   gammastep \
-  i3 \
   i3status-rust \
+  i3-wm \
   kdeconnect-kde \
+  libnotify \
   maim \
   NetworkManager-applet \
+  noto-fonts-emoji \
   picom \
   polkit \
   polkit-gnome \
   rofi \
   ttf-firacode-nerd \
+  ttf-font-awesome \
+  upower \
+  wezterm \
   xclip \
   xclipboard \
   xdg-desktop-portal \
