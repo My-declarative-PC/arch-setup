914e4c6a2a831672b1f2d9efe6b4be231f5047ef
Sitnikov Timofey <tima.sitnikov@mail.ru>
2025-03-15 16:17:04 +0300

    добавил настройку snapper-а

--- installers/snapper.sh
+++ installers/snapper.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Установка пакетов
 yay -S \
   snapper \
   btrfs-assistant \
   grub-btrfs \
   snap-pac \
   snap-pac-grub
+
+# Создание конфигурации для корневого раздела
+sudo snapper -c root create-config /
+
+# Настройка параметров снапшотов
+sudo snapper -c root set-config "TIMELINE_CREATE=yes" "TIMELINE_CLEANUP=yes"
+
+# Включение и запуск служб
+sudo systemctl enable --now snapper-timeline.timer
+sudo systemctl enable --now snapper-cleanup.timer
+sudo systemctl enable --now grub-btrfs.path
+
+echo "Настройка Snapper завершена!"
